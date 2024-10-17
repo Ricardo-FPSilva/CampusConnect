8e272a8f45ae825bb95fbb5f186bc2684ae282d9
Ricardo Silva <ricardo.silvamain@gmail.com>
2024-10-17 00:01:16 -0300

    New Screens

--- app/src/main/java/com/app/campusconnect/LoginScreen.kt
+++ app/src/main/java/com/app/campusconnect/LoginScreen.kt
@@ -1,25 +1,11 @@
 package com.app.campusconnect
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.app.campusconnect.ui.loginScreens.EnterProfileScreen
-import com.app.campusconnect.ui.loginScreens.InsertRegistrationScreen
-import com.app.campusconnect.ui.loginScreens.NewPasswordScreen
-import com.app.campusconnect.ui.loginScreens.VerificationCodeScreen
-import com.app.campusconnect.ui.loginScreens.WelcomeScreen
 import com.app.campusconnect.ui.theme.CampusConnectTheme
 
 @Composable
